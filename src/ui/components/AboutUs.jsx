12184526ffd76e6b6f0b85c38b003bcb1df7cecd
Rosa <rosaislp@gmail.com>
2020-07-03 13:26:32 -0500

    adding design to AboutUs Component

--- src/ui/components/AboutUs.jsx
+++ src/ui/components/AboutUs.jsx
@@ -1,10 +1,30 @@
 import Navbar from "./Navbar";
+import bakery from "../../static/img/bakery.jpg";
 
 const AboutUs = () => {
     return (
         <>
             <Navbar />
-            us
+            <div className="us-information">
+                <div className="container-title-us">
+                    <h1 className="title-us">
+                        <b>
+                            ¡TE AYUDAMOS A <br />
+                            REACTIVAR TU NEGOCIO!
+                        </b>
+                    </h1>
+                </div>
+            </div>
+            <div className="details-us">
+                Reworth es la red de recompensas más grande de la CDMX y una
+                iniciativa que te permite conectar sin costo y de forma
+                automática con miles de usuarios de nuestros bancos afiliados a
+                través de recompensas "cashback" en tu establecimiento. Para
+                conocer más da click{" "}
+                <b>
+                    <a href="https://www.reworth.co/">here</a>
+                </b>
+            </div>
         </>
     );
 };
