53e582bd0de169594bd5bde356de8e38948033b3
Rosa <rosaislp@gmail.com>
2020-07-03 01:23:57 -0500

    creating my data with url's images

---
+++ src/dataimg/items.js
@@ -0,0 +1,94 @@
+export default [
+    {
+        id: 1,
+        url: "https://unsplash.com/photos/yC-Yzbqy7PY",
+    },
+    {
+        id: 2,
+        url: "https://unsplash.com/photos/yC-Yzbqy7PY",
+    },
+    {
+        id: 3,
+        url: "https://unsplash.com/photos/7BjmDICVloE",
+    },
+    {
+        id: 4,
+        url: "https://unsplash.com/photos/6J--NXulQCs",
+    },
+    {
+        id: 5,
+        url: "https://unsplash.com/photos/1DkWWN1dr-s",
+    },
+    {
+        id: 6,
+        url: "https://unsplash.com/photos/tBtuxtLvAZs",
+    },
+    {
+        id: 7,
+        url: "https://unsplash.com/photos/LNRyGwIJr5c",
+    },
+    {
+        id: 8,
+        url: "https://unsplash.com/photos/-wWRHIUklxM",
+    },
+    {
+        id: 9,
+        url: "https://unsplash.com/photos/5L4XAgMSno0",
+    },
+    {
+        id: 10,
+        url: "https://unsplash.com/photos/8lMhzUjD1Wk",
+    },
+    {
+        id: 11,
+        url: "https://unsplash.com/photos/NaWKMlp3tVs",
+    },
+    {
+        id: 12,
+        url: "https://unsplash.com/photos/uAgLGG1WBd4",
+    },
+    {
+        id: 13,
+        url: "https://unsplash.com/photos/D8LcRLwZyPs",
+    },
+    {
+        id: 14,
+        url: "https://unsplash.com/photos/AmPRUnRb6N0",
+    },
+    {
+        id: 15,
+        url: "https://unsplash.com/photos/-oWyJoSqBRM",
+    },
+    {
+        id: 16,
+        url: "https://unsplash.com/photos/_h7aBovKia4",
+    },
+    {
+        id: 17,
+        url: "https://unsplash.com/photos/Kt5hRENuotI",
+    },
+    {
+        id: 18,
+        url: "https://unsplash.com/photos/pJILiyPdrXI",
+    },
+    {
+        id: 19,
+        url: "https://unsplash.com/photos/_snqARKTgoc",
+    },
+    {
+        id: 20,
+        url: "https://unsplash.com/photos/bClr95glx6k",
+    },
+    {
+        id: 21,
+        url: "https://unsplash.com/photos/U5rMrSI7Pn4",
+    },
+    {
+        id: 22,
+        url: "https://unsplash.com/photos/V6s1cmE39XM",
+    },
+    {
+        id: 23,
+        url: "https://unsplash.com/photos/7BjmDICVloE",
+    },
+];
--- src/ui/components/ImageList.jsx
+++ src/ui/components/ImageList.jsx
@@ -1,6 +1,8 @@
 import ImageCard from "./ImageCard";
 
 const ImageList = ({ imagess }) => {
+    console.log("images", imagess);
+
     const images = imagess.map(image => {
         return <ImageCard key={image.id} image={image} url={image[0].url} />;
     });
